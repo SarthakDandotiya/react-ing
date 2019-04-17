a5836cd26ea3711ea3ce807dd56972aff8f96bd2
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2019-04-17 15:04:12 +0530

    Deleting Component & Improving Code

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,14 +11,15 @@ class Counters extends Component {
 		]
 	};
 
-	handleDelete = () => {
-		console.log('called');
+	handleDelete = (counterId) => {
+		const counters = this.state.counters.filter(c => c.id !== counterId);
+		this.setState({ counters: counters })
 	};
 	render() {
 		return (
 			<div>
 				{this.state.counters.map(counter => (
-					<Counter key={counter.id} onDelete={this.handleDelete}  value={counter.value} />
+					<Counter key={counter.id} onDelete={this.handleDelete} counter={counter}/>
 				))}
 			</div>
 		);
