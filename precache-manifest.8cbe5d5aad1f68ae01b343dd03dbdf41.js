6bd70141c192a37e850fa39c7d8bfeeb59c6a9b3
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2019-04-21 16:23:58 +0530

    Updates

---
+++ precache-manifest.8cbe5d5aad1f68ae01b343dd03dbdf41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51ec1b80e7dbbc7abe29",
+    "url": "/react-ing/static/js/runtime~main.b6889c1e.js"
+  },
+  {
+    "revision": "1ce035442047bf578fd8",
+    "url": "/react-ing/static/js/main.d645249a.chunk.js"
+  },
+  {
+    "revision": "9e912aa37b393747ed9b",
+    "url": "/react-ing/static/js/2.eb088328.chunk.js"
+  },
+  {
+    "revision": "1ce035442047bf578fd8",
+    "url": "/react-ing/static/css/main.9d50a79e.chunk.css"
+  },
+  {
+    "revision": "9e912aa37b393747ed9b",
+    "url": "/react-ing/static/css/2.40bc1682.chunk.css"
+  },
+  {
+    "revision": "1834793a03d305b184aa48e3828e23c2",
+    "url": "/react-ing/index.html"
+  }
+];
